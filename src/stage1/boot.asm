b22145e95c9d9b54e1d24a28d3a9d00c589dcb04
Garry-os <baong2408@gmail.com>
2025-12-21 18:31:24 +0700

    Implemented disk reading

--- src/stage1/boot.asm
+++ src/stage1/boot.asm
@@ -42,6 +42,13 @@ start:
 	;; TODO: Print a message
 	jmp .halt
 .after:
+	; Test disk read
+	mov eax, 1
+	mov cl, 1
+	;; BIOS already set dl
+	mov bx, 0x1000
+	call disk_read
+
 	; A test
 	mov al, 'h'
 	mov ah, 0x0E
@@ -50,6 +57,63 @@ start:
 .halt:
 	cli
 	hlt
+
+;; Parameters:
+;;; EAX = LBA
+;;; CL = sectors count
+;;; DL: drive number
+;;; ES:BX: buffer location 
+disk_read:
+	;; Save registers
+	push eax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+
+	;; Setup extension DAP
+	mov [extension_dap.lba], eax
+	mov [extension_dap.segment], es
+	mov [extension_dap.offset], bx
+	mov [extension_dap.size], cl
+
+	mov ah, 0x42 ; Disk read
+	mov si, extension_dap
+.read:
+	pusha ; Save all registers
+	stc
+	int 0x13
+	jnc .done ; Succeed
+
+.fail:
+	;; Disk read failed
+	;; TODO: Print message
+	hlt
+	cli
+
+.done:
+	;; Restore registers
+	popa
+
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop eax
+
+	ret
+
+
+;; Extension data address packet structure
+extension_dap:
+	.size:    db 10
+		      db 0 ; Unused
+	.count:   dw 0
+	.offset:  dw 0
+	.segment: dw 0
+	.lba:     dq 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55 ;; BIOS magic
