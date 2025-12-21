9c810537211e2d435bb09b38942cbb27f3518211
Garry-os <baong2408@gmail.com>
2025-12-21 11:18:31 +0700

    Added checking for disk extension

--- src/boot.asm
+++ src/boot.asm
@@ -1,7 +1,19 @@
 org 0x7C00
 bits 16
 
-main:
+jmp short start
+nop
+
+;; FAT BPB
+;; ============ BPB bytes ===========
+;;
+
+start:
+	;; Resetting cs by performing a jump
+	cli
+	cld
+	jmp 0x000:.reset_cs
+.reset_cs:
 	;; Reset segments & setup stack
 	xor ax, ax
 	mov es, ax
@@ -9,6 +21,30 @@ main:
 
 	mov ss, ax
 	mov sp, 0x7C00 ; Stack grows downward
+	
+	sti
+	
+	;; Check disk extension
+	mov ah, 0x41
+	mov bx, 0x55AA
+	stc
+	int 0x13
+
+	jc .no_disk_extension
+
+	;; Verify
+	cmp bx, 0xAA55
+	jne .no_disk_extension
+	
+	jmp .after
+.no_disk_extension:
+	;; TODO: Print a message
+	jmp .halt
+.after:
+	; A test
+	mov al, 'h'
+	mov ah, 0x0E
+	int 0x10
 
 .halt:
 	cli
