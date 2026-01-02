6d7d13624e9761a94d025fac7c0ca744e891dccf
Garry-os <baong2408@gmail.com>
2026-01-02 10:48:15 +0700

    Fixed stage2 truncate the disk image

--- scripts/make_disk.sh
+++ scripts/make_disk.sh
@@ -21,7 +21,7 @@ echo "Copying files"
 dd if=../build/stage1/boot.bin of=../build/disk.img bs=1 seek=0 count=3 conv=notrunc
 dd if=../build/stage1/boot.bin of=../build/disk.img bs=1 seek=138 skip=138 conv=notrunc
 
-dd if=../build/stage2/stage2.bin of=../build/disk.img bs=512 seek=1
+dd if=../build/stage2/stage2.bin of=../build/disk.img bs=512 seek=1 conv=notrunc
 
 echo "Finished"
 
