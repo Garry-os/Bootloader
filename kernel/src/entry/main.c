0757c97a693fc8b4055f39d6a8f016caabb951da
Garry-os <baong2408@gmail.com>
2026-01-11 11:05:06 +0700

    Implemented I/O ports outputting

--- kernel/src/entry/main.c
+++ kernel/src/entry/main.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 #include <console.h>
+#include <x86_64/system.h>
 
 void main()
 {
 	clearScreen();
 
+	x86_outb(0xE9, 'h');
 	puts("Hello world from kernel!\n");
 
 	while (1);
