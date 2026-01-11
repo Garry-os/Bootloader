d0cd5493dfeaf73070f97b938de202b8ea8eeabd
Garry-os <baong2408@gmail.com>
2026-01-11 17:58:10 +0700

    Add ASMCALL to the ASM functions

--- bootloader/stage2/src/x86.h
+++ bootloader/stage2/src/x86.h
@@ -7,6 +7,6 @@ void ASMCALL x86_Test();
 //
 // Disk operations
 // 
-bool x86_DiskReset(uint8_t drive);
-bool x86_DiskRead(uint8_t drive, uint32_t lba, uint8_t sectorCount, void* dataOut);
+bool ASMCALL x86_DiskReset(uint8_t drive);
+bool ASMCALL x86_DiskRead(uint8_t drive, uint32_t lba, uint8_t sectorCount, void* dataOut);
 
